1e1ff3c29dcbcd3ac4a6ba09f90f4ebf44c0cc95
MullerCedric <muller.cedric.96@gmail.com>
2018-01-21 10:22:17 +0100

    Update nav with Redux

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import reducers from './src/reducers';
 
 import firebase from 'firebase';
 import { firebaseConfig } from './settings';
 
+import { addNavigationHelpers } from 'react-navigation';
 import Navigator from './src/config/routes';
+
+const AppNav = ({ dispatch, nav }) => (
+  <Navigator
+    navigation={addNavigationHelpers({
+      dispatch,
+      state: nav,
+    })}
+  />
+);
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+const AppNavigation = connect(mapStateToProps)(AppNav);
 
 export default class App extends Component {
   componentWillMount(){
@@ -18,7 +34,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <Navigator onNavigationStateChange={null} />
+        <AppNavigation />
       </Provider>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+
+import nav from './nav';
 
 export default combineReducers({
-  reducerName: () => []
-})
+  nav,
+});
