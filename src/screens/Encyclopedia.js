4831185e3532874f0156c59fb2c7e31dd95cf73d
MullerCedric <muller.cedric.96@gmail.com>
2018-01-24 18:39:01 +0100

    Update ListView by FlatList

--- src/screens/Encyclopedia.js
+++ src/screens/Encyclopedia.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ListView, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, FlatList, TouchableWithoutFeedback } from 'react-native';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { infosFetch } from '../actions';
@@ -8,49 +8,28 @@ import { CardSection } from '../components';
 class Encyclopedia extends Component {
   componentWillMount() {
     this.props.infosFetch();
-
-    this.createDataSource(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ infos }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(infos);
   }
 
   onRowPress (bird) {
     this.props.navigation.navigate('AboutThatBird', { ...bird });
   }
 
-  renderRow(info) {
-    const { commonName } = info;
-
-    return (
-      <TouchableWithoutFeedback onPress={() => this.onRowPress(info)}>
-        <View>
-          <CardSection>
-            <Text style={styles.titleStyle}>
-              {commonName}
-            </Text>
-          </CardSection>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  }
-
   render() {
     return (
-      <ListView
-        enableEmptySections
-        dataSource={this.dataSource}
-        renderRow={this.renderRow.bind(this)}
-      />/////
+      <FlatList
+        data={this.props.infos}
+        renderItem={({ item }) => (
+          <TouchableWithoutFeedback onPress={() => this.onRowPress(item)}>
+            <View>
+              <CardSection>
+                <Text style={styles.titleStyle}>
+                  {item.commonName}
+                </Text>
+              </CardSection>
+            </View>
+          </TouchableWithoutFeedback>
+        )}
+      />
     );
   }
 }
